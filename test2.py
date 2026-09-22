5b665d6da29060c0bb503363d80ac3b84f8de198
Роман Кунгурцев <kungurtsev5roman07@gmail.com>
2026-09-22 16:23:36 +0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -193,7 +193,7 @@ def check_task1(user_func, github_token: str = None, repo_owner: str = None, rep
         for err in perf_errors:
             print(f"   • {err}")
     else:
-        print("🎉 ВСЕ ТЕСТЫ И БЕНЧМАРКИ УСПЕШНО ПРОЙДЕНЫ!")
+        print(f"🎉 ВСЕ ТЕСТЫ И БЕНЧМАРКИ УСПЕШНО ПРОЙДЕНЫ! {select_count}")
 
         # Добавляем вызов отправки (передаем токены, если они доступны в вашей программе)
         # Допустим, github_token, repo_owner и repo_name передаются глобально или как аргументы
